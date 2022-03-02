37d73d40c1fddc072639ce0cd18e118ba1930eea
Hasanat902 <abulhasanat902@gmail.com>
2022-03-02 19:34:06 +0600

    display weather information

--- js/app.js
+++ js/app.js
@@ -4,10 +4,26 @@ const searchWeather = () => {
 
     const city = document.getElementById("input-field").value;
     
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayWeather(data))
 
         document.getElementById("input-field").value = '';
+}
+
+const getInnerText = (id, value) => {
+    document.getElementById(id).innerText = value;
+}
+
+const displayWeather = temperature => {
+    
+    getInnerText("city", temperature.name);
+    getInnerText("temp", temperature.main.temp);
+    getInnerText("condition", temperature.weather[0].main);
+
+    const url = ` http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const weatherIcon = document.getElementById("weather-icon");
+    weatherIcon.setAttribute("src", url);
+    console.log(temperature);
 }
